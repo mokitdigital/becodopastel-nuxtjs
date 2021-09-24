b801ad28d3a289c07e44010acf8f6babfe577110
Mokit Digital <78105125+mokitdigital@users.noreply.github.com>
2021-09-23 21:51:55 -0300

    Versão 0.1.2: Feito alterações no nuxt.config para poder processar o babel de forma sem exceder o tamanho do build

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,10 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap' }
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Lexend:wght@100;200;300;400;500;600;700;800;900&display=swap'
+      }
     ]
   },
 
@@ -33,13 +36,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: [
-    '~/components'
-  ],
+  components: ['~/components'],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
@@ -60,8 +60,7 @@ export default {
     'nuxt-sweetalert2',
     '@nuxtjs/auth-next'
   ],
-  auth: {
-  },
+  auth: {},
 
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {},
@@ -75,5 +74,8 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    babel: {
+      compact: true
+    }
   }
 }
